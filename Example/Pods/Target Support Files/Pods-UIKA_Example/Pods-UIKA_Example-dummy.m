cabc68d2b3e8e66fd133b189834e5d2228cc043c
Krishna Venkatramani <krishna@getbaraka.com>
2023-06-14 22:52:12 +0400

    Init

---
+++ Example/Pods/Target Support Files/Pods-UIKA_Example/Pods-UIKA_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_UIKA_Example : NSObject
+@end
+@implementation PodsDummy_Pods_UIKA_Example
+@end
---
+++ Example/Pods/Target Support Files/UIKA/UIKA-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_UIKA : NSObject
+@end
+@implementation PodsDummy_UIKA
+@end
